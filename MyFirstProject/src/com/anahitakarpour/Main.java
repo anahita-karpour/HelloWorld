80c2e2ac7aee71fb6296d3680396f2d3591b5755
anahita-karpour <anahita.karpour@gmail.com>
2020-03-08 21:27:30 +1300

    Reverting back the comment

--- MyFirstProject/src/com/anahitakarpour/Main.java
+++ MyFirstProject/src/com/anahitakarpour/Main.java
@@ -3,7 +3,7 @@ package com.anahitakarpour;
 public class Main {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// TODO Auto-generated method stub
         System.out.println("Hello World");
 	}
 
